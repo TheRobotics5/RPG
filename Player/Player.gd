a926e527b3f432d9ce31e37026d0edec8e3553fc
Ethan Karp <ethankarp5@gmail.com>
2021-01-07 11:40:27 -0800

    part 2

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,20 +1,21 @@
 extends KinematicBody2D
 
+const ACCEL = 50
+const MAXSPEED = 130
 const SPEED = 1
+const FRIC = 220
 var velocity = Vector2.ZERO
 
 func _physics_process(delta):
-	if Input.is_action_pressed("ui_right"):
-		velocity.x = SPEED
-	elif Input.is_action_pressed("ui_left"):
-		velocity.x = -SPEED
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	
+	if input_vector != Vector2.ZERO:
+		velocity += input_vector * ACCEL * delta
+		velocity = velocity.clamped(MAXSPEED * delta)
 	else:
-		velocity.x = 0
-	if Input.is_action_pressed("ui_up"):
-		velocity.y = -SPEED
-	elif Input.is_action_pressed("ui_down"):
-		velocity.y = SPEED
-	else:
-		velocity.y = 0
+		velocity = velocity.move_toward(Vector2.ZERO, FRIC * delta)
 
 	move_and_collide(velocity)
