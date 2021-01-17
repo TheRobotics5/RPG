4bafe6055b660c6ac009ad81e2606b9e7b8dbbc0
Ethan Karp <ethankarp5@gmail.com>
2021-01-17 11:07:11 -0800

    part 4 & 5

--- Player/Player.gd
+++ Player/Player.gd
@@ -4,6 +4,7 @@ const ACCEL = 350
 const MAXSPEED = 100
 const FRIC = 540
 var velocity = Vector2.ZERO
+onready var animPlayer = $AnimationPlayer
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
@@ -11,9 +12,16 @@ func _physics_process(delta):
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
 	
+	
 	if input_vector != Vector2.ZERO:
+		if input_vector.x > 0:
+			animPlayer.play("RunRight")
+		else:
+			animPlayer.play("RunLeft")
+		
 		velocity = velocity.move_toward(input_vector * MAXSPEED, ACCEL * delta)
 	else:
+		animPlayer.play("IdleRight")
 		velocity = velocity.move_toward(Vector2.ZERO, FRIC * delta)
 
 	velocity = move_and_slide(velocity)
