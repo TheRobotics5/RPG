f2ec166e893f55ebb7effd92259016df4e17da52
Ethan Karp <ethankarp5@gmail.com>
2021-01-23 16:03:05 -0800

    part 6

--- Player/Player.gd
+++ Player/Player.gd
@@ -2,9 +2,11 @@ extends KinematicBody2D
 
 const ACCEL = 350
 const MAXSPEED = 100
-const FRIC = 540
+const FRIC = 600
 var velocity = Vector2.ZERO
 onready var animPlayer = $AnimationPlayer
+onready var animTree = $AnimationTree
+onready var animState = animTree.get("parameters/playback")
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
@@ -14,14 +16,12 @@ func _physics_process(delta):
 	
 	
 	if input_vector != Vector2.ZERO:
-		if input_vector.x > 0:
-			animPlayer.play("RunRight")
-		else:
-			animPlayer.play("RunLeft")
-		
+		animTree.set("parameters/Idle/blend_position", input_vector)
+		animTree.set("parameters/Run/blend_position", input_vector)
+		animState.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAXSPEED, ACCEL * delta)
 	else:
-		animPlayer.play("IdleRight")
+		animState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRIC * delta)
 
 	velocity = move_and_slide(velocity)
